5043f5653bd114c8403ad1cb911ebb7645fbb0f4
Wectornanime <wectornanime@gmail.com>
2025-06-19 00:18:50 -0300

    feat: Add HomePage

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { PokemonListResponse } from 'src/app/models/poke-api.models';
+import { PokeApiService } from 'src/app/services/poke-api/poke-api.service';
+import { PokemonCardComponent } from 'src/app/components/pokemon-card/pokemon-card.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonCardComponent, NgFor, NgIf],
 })
-export class HomePage {
-  constructor() {}
+
+export class HomePage implements OnInit {
+  pokemonList: PokemonListResponse | null = null;
+
+  constructor(private pokeApi: PokeApiService) {}
+
+  ngOnInit(): void {
+    this.pokeApi.getPokemonList({
+      offset: 0, limit: 24
+    }).subscribe(result => this.pokemonList = result)
+  }
 }
