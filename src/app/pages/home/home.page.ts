3543d99ecd74182e11a16d559b2f3ac7b30f1dcb
Wectornanime <wectornanime@gmail.com>
2025-06-20 16:18:05 -0300

    feat: Add infinite scroll
    This commit adds infinite scrolling to the home page, previously the
    list of Pokémon here was a maximum of 24, now it is possible to have
    them all.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { PokemonListResponse } from 'src/app/models/poke-api.models';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInfiniteScroll, IonInfiniteScrollContent } from '@ionic/angular/standalone';
 import { PokeApiService } from 'src/app/services/poke-api/poke-api.service';
 import { PokemonCardComponent } from 'src/app/components/pokemon-card/pokemon-card.component';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonCardComponent, NgFor, NgIf, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonCardComponent, NgFor, RouterLink, IonInfiniteScroll, IonInfiniteScrollContent],
 })
 
 export class HomePage implements OnInit {
-  pokemonList: PokemonListResponse | null = null;
+  pokemonList: { name: string; url: string }[] = [];
+  offset: number = 0;
+  limit: number = 24;
+  isLoading: boolean = false;
 
-  constructor(private pokeApi: PokeApiService) {}
+  constructor(private pokeApi: PokeApiService) { }
 
   ngOnInit(): void {
+    this.loadPokemons();
+  }
+
+  loadPokemons(event?: any): void {
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+
     this.pokeApi.getPokemonList({
-      offset: 0, limit: 24
-    }).subscribe(result => this.pokemonList = result)
+      offset: this.offset,
+      limit: this.limit
+    }).subscribe(result => {
+      this.pokemonList?.push(...result.results);
+      this.offset += this.limit;
+
+      this.isLoading = false;
+
+      event?.target?.complete();
+    })
   }
 }
