c44da5e61e2afc58c75d2d3601ff2c046ccf2e7f
Wectornanime <wectornanime@gmail.com>
2025-06-27 11:10:10 -0300

    feat(test): Add tests of pokemon-card
    This commit adds a suite test for pokemon-card-component. Are being
    tested the creation and the dynamic background color of the component.

--- src/app/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/components/pokemon-card/pokemon-card.component.spec.ts
@@ -1,7 +1,12 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { PokemonCardComponent } from './pokemon-card.component';
+import { NgFor } from '@angular/common';
+import { PokemonTypeCardComponent } from '../pokemon-type-card/pokemon-type-card.component';
+import { getColor } from 'src/app/utils/pokemon-type-color-map';
 
 describe('PokemonCardComponent', () => {
   let component: PokemonCardComponent;
@@ -9,16 +14,38 @@ describe('PokemonCardComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PokemonCardComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        PokemonCardComponent,
+        PokemonTypeCardComponent,
+        NgFor,
+        IonicModule.forRoot(),
+        HttpClientTestingModule,
+      ],
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PokemonCardComponent);
     component = fixture.componentInstance;
+
+    component.pokemonName = 'pikachu';
+    component.ngOnInit();
+
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set the background color according to the pokemon type', () => {
+    const cardColor = 'rgb(247, 207, 46)';
+    const pokemonType = 'electric';
+
+    component.bgColor = getColor(pokemonType);
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.nativeElement;
+    expect(element.style.backgroundColor).toEqual(cardColor)
+  })
 });
